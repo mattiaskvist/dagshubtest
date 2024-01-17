b640e638b6d436b12632e1438591572bc6e0e9af
Mattias Kvist <makv@kth.se>
2024-01-17 01:03:33 +0100

    Add first test of mlflow integration with DagsHub

--- README.md
+++ README.md
@@ -1,2 +1,62 @@
 # dagshub-test
+
 Test för dagshub integration
+
+## Installation
+
+```bash
+
+pip install virtualenv
+python3.11 -m venv env
+```
+
+För att aktivera enviren:
+
+```bash
+//mac
+source env/bin/activate
+
+//CMD
+env\Scripts\activate.bat
+
+//Powershell
+env\Scripts\activate.ps1
+```
+
+För att installera paket:
+
+```bash
+pip install -r requirements.txt
+```
+
+För att deaktivera enviren:
+
+```bash
+deactivate
+```
+
+De paket som installeras är:
+
+```bash
+pip install mlflow 
+pip install dagshub
+pip install xgboost
+pip install scikit-learn
+pip install numpy
+pip install python-dotenv 
+```
+
+Detta behövs bara ifall man inte kör i en environment.
+
+## Hämta data
+
+Ladda ner data här: [diabetes](https://raw.githubusercontent.com/jbrownlee/Datasets/master/pima-indians-diabetes.data.csv). Kopiera och lägg i repot som 'pima-indians-diabetes.data.csv'.
+
+## Set up credentials
+
+Ditt username är ditt användarnamn på dagshub och password är ditt token. Detta kan du hitta på dagshub under settings långt ner.
+
+```bash
+export MLFLOW_TRACKING_USERNAME=<username>
+export MLFLOW_TRACKING_PASSWORD=<password/token>
+```
---
+++ xgboost_test.py
@@ -0,0 +1,55 @@
+# Load environment variables
+from dotenv import load_dotenv
+load_dotenv()
+
+import os
+repo_name_var = os.getenv("DAGSHUB_REPO_NAME")
+repo_owner_var = os.getenv("DAGSHUB_REPO_OWNER")
+tracking_uri_var = os.getenv("MLFLOW_TRACKING_URI")
+dagshub_token_var = os.getenv("DAGSHUB_TOKEN")
+dagshub_username_var = os.getenv("DAGSHUB_USERNAME")
+
+os.environ['MLFLOW_TRACKING_USERNAME'] = dagshub_username_var
+os.environ['MLFLOW_TRACKING_PASSWORD'] = dagshub_token_var
+
+# Set up Dagshub and MLflow
+import dagshub
+import mlflow
+
+dagshub.init(repo_owner=repo_owner_var, repo_name=repo_name_var, mlflow=True)
+mlflow.set_tracking_uri(tracking_uri_var)
+with mlflow.start_run():
+    # Load libraries
+    from numpy import loadtxt
+    from xgboost import XGBClassifier
+    from sklearn.model_selection import train_test_split
+    from sklearn.metrics import accuracy_score
+
+    # load data
+    dataset = loadtxt('pima-indians-diabetes.csv', delimiter=",")
+
+    # split data into X and y
+    X = dataset[:,0:8]
+    Y = dataset[:,8]
+
+    # split data into train and test sets
+    seed = 7
+    test_size = 0.33
+    X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=test_size, random_state=seed)
+
+    # fit model on training data
+    model = XGBClassifier()
+    model.fit(X_train, y_train)
+
+    # make predictions for test data
+    y_pred = model.predict(X_test)
+    predictions = [round(value) for value in y_pred]
+
+    # evaluate predictions
+    accuracy = accuracy_score(y_test, predictions)
+    print("Accuracy: %.2f%%" % (accuracy * 100.0))
+
+    mlflow.log_param('accuracy', accuracy)
+    mlflow.log_metric("metric name", 1)
+
+    mlflow.end_run()
